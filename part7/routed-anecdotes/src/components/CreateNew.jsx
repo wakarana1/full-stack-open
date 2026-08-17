eb83f7b4e8f8daec645b4217f672b0548c5aecd4
wakarana1 <wu.christophert@gmail.com>
2026-08-16 23:58:12 -0700

    part7 start

---
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,46 @@
+import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
+
+const CreateNew = ({ addAnecdote }) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addAnecdote({ content: content.value, author: author.value, info: info.value, votes: 0 })
+    navigate('/')
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name='content' type={content.type} value={content.value} onChange={content.onChange} />
+        </div>
+        <div>
+          author
+          <input name='author' type={author.type} value={author.value} onChange={author.onChange} />
+        </div>
+        <div>
+          url for more info
+          <input name='info' type={info.type} value={info.value} onChange={info.onChange} />
+        </div>
+        <button type='submit'>create</button>
+        <button type='button' onClick={handleReset}>reset</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
